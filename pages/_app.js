63754031dac7394cd0358f9b2d0a08ac181adbfa
serra <serra@boten.ca>
2022-12-05 15:31:26 -0800

    add supabase auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import { useState } from 'react'
 import '../styles/globals.css'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+  return (
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
 }
 
 export default MyApp
