ddfab251fb4bc5591e2d354827a0026a17d29356
serra <serra@boten.ca>
2022-12-05 12:17:46 -0800

    first commit!

---
+++ db/trips.sql
@@ -0,0 +1,3 @@
+INSERT INTO trips (trip_name, trip_description, start_date, end_date, user_id) VALUES ('Seattle 2012', 'We drove to Seattle to see Radiohead.', '2012-07-20T10:20:00.00Z', '2012-07-07T13:31:00.00Z', 1);
+INSERT INTO trips (trip_name, trip_description, start_date, end_date, user_id) VALUES ('Bikeymoon', 'Riding bikes across Europe', '2014-09-20T10:20:00.00Z', '2014-10-17T13:31:00.00Z', 1);
+
